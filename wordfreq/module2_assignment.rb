2535f7412604382e3b2d77894f7549615e650fc2
gary boland <garyboland@gmail.com>
2017-02-17 23:46:30 +0000

    working assignment2. a fun way to spend friday night

--- wordfreq/module2_assignment.rb
+++ wordfreq/module2_assignment.rb
@@ -1,14 +1,5 @@
-#Implement all parts of this assignment within (this) module2_assignment2.rb file
-
-#Implement a class called LineAnalyzer.
 class LineAnalyzer
   attr_reader :highest_wf_count, :highest_wf_words, :content, :line_number
-  #Implement the following read-only attributes in the LineAnalyzer class. 
-  #* highest_wf_count - a number with maximum number of occurrences for a single word (calculated)
-  #* highest_wf_words - an array of words with the maximum number of occurrences (calculated)
-  #* content          - the string analyzed (provided)
-  #* line_number      - the line number analyzed (provided)
-
   def initialize(line, num)
     @content = line
     @line_number = num
@@ -16,32 +7,45 @@ class LineAnalyzer
   end
 
   def calculate_word_frequency
-    wf = Hash.new(0).tap { |h| @content.split.each { |word| h[word] += 1 } }
+    wf = Hash.new(0).tap { |h| @content.split.each { |word| h[word.downcase] += 1 } }
     fw = wf.each_with_object({}){ |i,o|k,v = *i; o[v] ||=[]; o[v] << k}
     @highest_wf_count = fw.max[0]
     @highest_wf_words = fw.max[1]
   end
-  #Add the following methods in the LineAnalyzer class.
-  #* initialize() - taking a line of text (content) and a line number
-  #* calculate_word_frequency() - calculates result
+end
+class Solution
+  attr_reader :analyzers, :highest_count_across_lines, :highest_count_words_across_lines
+  def initialize
+    @analyzers = []
+  end
 
-  #Implement the initialize() method to:
-  #* take in a line of text and line number
-  #* initialize the content and line_number attributes
-  #* call the calculate_word_frequency() method.
+  def analyze_file
+    n = 0
+    data = File.read('test.txt')
+    #require 'pry';binding.pry
+    data.split("\n").each do |x|
+      @analyzers << LineAnalyzer.new(x, n)
+      n += 1
+    end
+  end
 
-  #Implement the calculate_word_frequency() method to:
-  #* calculate the maximum number of times a single word appears within
-  #  provided content and store that in the highest_wf_count attribute.
-  #* identify the words that were used the maximum number of times and
-  #  store that in the highest_wf_words attribute.
+  def calculate_line_with_highest_frequency
+    @highest_count_across_lines = @analyzers.max_by(&:highest_wf_count).highest_wf_count
+    @highest_count_words_across_lines = []
+    @highest_count_words_across_lines += @analyzers.map {|x| x if x.highest_wf_count == @highest_count_across_lines}.compact.flatten
+    #require 'pry';binding.pry
+  end
+
+  def print_highest_word_frequency_across_lines
+  end
 end
 
 #  Implement a class called Solution. 
-class Solution
+#Implement all parts of this assignment within (this) module2_assignment2.rb file
 
+#Implement a class called LineAnalyzer.
   # Implement the following read-only attributes in the Solution class.
-  #* analyzers - an array of LineAnalyzer objects for each line in the file
+  #* analyzers - an array of LineAnalyzer objects for each line in the filekkkk
   #* highest_count_across_lines - a number with the maximum value for highest_wf_words attribute in the analyzers array.
   #* highest_count_words_across_lines - a filtered array of LineAnalyzer objects with the highest_wf_words attribute 
   #  equal to the highest_count_across_lines determined previously.
@@ -65,4 +69,43 @@ class Solution
 
   #Implement the print_highest_word_frequency_across_lines() method to
   #* print the values of objects in highest_count_words_across_lines in the specified format
-end
+  #Implement the following read-only attributes in the LineAnalyzer class. 
+  #* highest_wf_count - a number with maximum number of occurrences for a single word (calculated)
+  #* highest_wf_words - an array of words with the maximum number of occurrences (calculated)
+  #* content          - the string analyzed (provided)
+  #* line_number      - the line number analyzed (provided)
+
+  #Add the following methods in the LineAnalyzer class.
+  #* initialize() - taking a line of text (content) and a line number
+  #* calculate_word_frequency() - calculates result
+
+  #Implement the initialize() method to:
+  #* take in a line of text and line number
+  #* initialize the content and line_number attributes
+  #* call the calculate_word_frequency() method.
+
+  #Implement the calculate_word_frequency() method to:
+  #* calculate the maximum number of times a single word appears within
+  #  provided content and store that in the highest_wf_count attribute.
+  #* identify the words that were used the maximum number of times and
+  #  store that in the highest_wf_words attribute.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
