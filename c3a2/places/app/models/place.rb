3c22878b08516c63e098e0facf5d5812c2a53af1
gary boland <garyboland@gmail.com>
2017-08-17 00:03:38 +0100

    rq05

--- c3a2/places/app/models/place.rb
+++ c3a2/places/app/models/place.rb
@@ -1,5 +1,12 @@
 class Place
   include Mongoid::Document
+  attr_accessor :id, :formatted_address, :location, :address_components
+  def initialize(h)
+    @id = h[:_id].to_s
+    @address_components = h[:address_components].collect { |a| AddressComponent.new(a) }
+    @formatted_address = h[:formatted_address]
+    @location = Point.new(h[:geometry][:location])
+  end
   def self.mongo_client
     Mongoid::Clients.default
   end
